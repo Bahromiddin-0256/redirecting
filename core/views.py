8daae539aa5de774ec9ebbf730f5d210775cdd46
b.ibragimov <b.ibragimov@uicgroup.org>
2022-11-24 21:03:13 +0500

    initial

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ import requests
 
 
 class CustomSchemeRedirect(HttpResponsePermanentRedirect):
-    allowed_schemes = ['munirapp']
+    allowed_schemes = ['munirapp', 'https', 'http', 'ftp', 'ftps']
 
 
 class IndexView(TemplateView):
@@ -14,5 +14,7 @@ class IndexView(TemplateView):
         res = requests.get('http://munir-admin.xn--h28h.uz/api/v1/audio_links/retrieve/?link=http://listen.bookmedianashr.uz/shif/index.html?p=1')
         if res.status_code == 200:
             slug = res.json()['product_slug']
-            return CustomSchemeRedirect(f'munirapp://org.uicgroup.munir/product/{slug}')
+            print(slug, f'https://org.uicgroup.munir/product/{slug}')
+            return CustomSchemeRedirect(f'https://org.uicgroup.munir/product/{slug}')
+        return super().get(request, *args, **kwargs)
 
