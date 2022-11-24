3f7bc558dc9c6900efbd4911296459f319679f13
b.ibragimov <b.ibragimov@uicgroup.org>
2022-11-24 20:55:15 +0500

    initial

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h^+^&1gek=l6jkl2-uzkh-d*@jtsmmytls&%^av9snj0d^gp6o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
