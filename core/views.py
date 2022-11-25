607469122806931c87527653fdc599bff9c8c9cd
b.ibragimov <b.ibragimov@uicgroup.org>
2022-11-25 15:44:41 +0500

    initial

--- core/views.py
+++ core/views.py
@@ -18,14 +18,13 @@ class IndexView(TemplateView):
         api = f'http://munir-admin.xn--h28h.uz/api/v1/audio_links/retrieve/'
         context['api'] = api
         context['url'] = url
-        context['red_url'] = 'munirapp://books/product_slug/'
+        context['red_url'] = 'munirapp://audiobook/product_slug/'
         res = requests.get(f'http://munir-admin.xn--h28h.uz/api/v1/audio_links/retrieve/?link={url}')
         if res.status_code == 200:
             context['data'] = mark_safe(json.dumps(res.json()))
         return context
 
     def get(self, request, *args, **kwargs):
-
         return super().get(request, *args, **kwargs)
 
 
